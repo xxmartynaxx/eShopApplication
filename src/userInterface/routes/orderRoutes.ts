3e116fb1ccb67f1b6c208dcc518a201237eb5b41
Martyna Mielczarek <martynamielczarek16.12@gmail.com>
2025-01-22 14:13:18 +0100

    Views Schemas are being created

--- src/userInterface/routes/orderRoutes.ts
+++ src/userInterface/routes/orderRoutes.ts
@@ -1,9 +1,20 @@
+// NA RAZIE NIE WIEM JAK TO SENSOWNIE ZROBIĆ
 import { Router } from "express";
 import { OrderService } from "../../application/orderService";
 
 const router = Router();
 const orderService = new OrderService(); 
 
-// metody na podstawie userRoutes.ts
+// POST /orders – Obsługa formularza złożenia zamówienia
+router.post('/createOder', async (req, res) => {
+    const { cartId } = req.body; // skąd pobierać id koszyka?
+    const response = await orderService.createOrder(cartId);
+
+    if (response.success) {
+        res.redirect('/cart'); 
+    } else {
+        res.render('cartViews/getCart', { title: 'Cart', error: response.message });
+    }
+});
 
 export { router as orderRoutes };
